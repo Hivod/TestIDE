358a7bb6f2ac8447ee6d8d7684743c936437ceda
Olof Rydh <te16.olof.rydh@umea.nti.se>
2017-11-06 10:59:12 +0100

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,26 @@
+/*  Slumpar en int mellan 1-100 och låter användaren gissa numret
+    Olof Rydh
+    2017-11-06
+*/
+
 package com.company;
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world!");
+        int randomNumber = (int) Math.ceil(Math.random()*100);
+        int guess = 0;
+        while(guess < 1 || guess > 100) {
+            guess = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1-100"));
+        }
+
+        if(guess == randomNumber) {
+            JOptionPane.showMessageDialog(null, "Correct");
+        } else if(guess > randomNumber) {
+            JOptionPane.showMessageDialog(null, "Too large");
+        } else {
+            JOptionPane.showMessageDialog(null, "Too small");
+        }
     }
 }
